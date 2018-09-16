2d5eb5f9def2e2777eeec42489b3683f23d66a31
Lucas Mzz <lucasderh@gmail.com>
2018-09-16 16:09:14 +0000

    adding search functionality

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const weather = require("weather-js");
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req,res) => {
     res.render('pages/home.ejs');
@@ -16,6 +19,18 @@ app.get('/:city',(req,res) => {
         res.render('pages/weather.ejs',{forecast: result});
     });
 });
+
+app.post('/weather',(req,res) => {
+    
+    weather.find({search: req.body.city, degreeType: 'F'}, function(err, result) {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('pages/weather.ejs',{forecast: result});  
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP,() => {
     console.log(`Server started on port ${process.env.IP}:${process.env.PORT}`);
