d2fafd33f24ee277b194bf5198ac6fb5629e381b
Lucas Mzz <lucasderh@gmail.com>
2018-09-17 15:43:50 +0000

    adding more changes to the formatting

--- app.js
+++ app.js
@@ -10,19 +10,9 @@ app.get('/',(req,res) => {
     res.render('pages/home.ejs');
 });
 
-app.get('/:city',(req,res) => {
-    
-    const city = req.params.city;
-    
-    weather.find({search: city, degreeType: 'F'}, function(err, result) {
-      if(err) console.log(err);
-        res.render('pages/weather.ejs',{forecast: result});
-    });
-});
-
 app.post('/weather',(req,res) => {
     
-    weather.find({search: req.body.city, degreeType: 'F'}, function(err, result) {
+    weather.find({search: req.body.city, degreeType: 'F'}, (err, result) => {
         if(err){
             console.log(err);
         }else{
