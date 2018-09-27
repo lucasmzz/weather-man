d4d33825c0ed57fa692582ad325e3db76134eb7e
Lucas Mzz <lucasderh@gmail.com>
2018-09-27 18:22:24 +0000

    merging changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const weather = require("weather-js");
 const bodyParser = require('body-parser');
-
+const path = require("path");
 const app = express();
+
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/',(req,res) => {
     res.render('pages/home.ejs');
